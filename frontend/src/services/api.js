4c03e402c4e9ce60d7c76a5942ab60f8ba795348
PavanUmmagani07 <pavankalyanU@outlook.com>
2025-06-21 23:26:54 +0530

    edits

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = "https://medibuddy-9kfe.onrender.com/api";
 
 const getAuthHeaders = () => {
   const token = localStorage.getItem('token');
